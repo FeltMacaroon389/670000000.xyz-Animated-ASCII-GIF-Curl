af4d5ee49320e6a2c2a3ede172794252cd48c065
FeltMacaroon389 <feltmacaroon@proton.me>
2026-03-10 14:53:02 +0100

    add 67 kid frames

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
+const fs = require("fs")
 const http = require("http")
 
-const frames = ["(>'-')>", "<('-'<)", "^('-')^"]
+const frames = fs.readdirSync("./ascii")
+  .sort()
+  .map(f => fs.readFileSync("./ascii/" + f, "utf8"))
 
 http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" })
+
+  const ua = req.headers["user-agent"] || ""
+
+  // browser response
+  if (!ua.includes("curl")) {
+    res.writeHead(200, {"Content-Type":"text/html"})
+    res.end(`
+	  <!DOCTYPE html>
+      <html>
+      <body>
+      <h1>67 :D</h1>
+      </body>
+      </html>
+    `)
+    return
+  }
+
+  // curl response
+  res.writeHead(200, {"Content-Type":"text/plain"})
 
   let i = 0
 
   setInterval(() => {
-    res.write("\x1b[H")
-    res.write(frames[i] + "\n")
+    res.write("\x1b[H")        // move cursor to top
+    res.write(frames[i])
     i = (i + 1) % frames.length
-  }, 100)
+  }, 80)
 
 }).listen(process.env.PORT || 3000)
